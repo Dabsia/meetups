ee76bfe202b7ab80b5c946ffe0b02e5d13140614
Dabsia <dabojohnson@gmail.com>
2022-05-15 15:43:24 -0700

    Created a meetups app with next Js

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import NewMeetUpForm from '../../components/meetups/NewMeetupForm'
+
+const addMeetUpHandler = (meetupData) => {
+    console.log(meetupData)
+}
+
+
+const NewMeetUpPage = () => {
+    return (
+      <div>
+        <Head>
+            <title>Create New-meetup</title>
+        </Head>
+        <NewMeetUpForm onAddMeetup =  {addMeetUpHandler} />
+      </div>
+  )
+}
+
+export default NewMeetUpPage
